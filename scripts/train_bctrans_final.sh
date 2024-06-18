11333e92c2291bd8c968c10b66b3aa4cea40cd20
Atharva Anil Mete <amete7@atl1-1-02-004-25-0.pace.gatech.edu>
2024-06-18 16:32:40 -0400

    changes

--- scripts/train_bctrans_final.sh
+++ scripts/train_bctrans_final.sh
@@ -18,22 +18,22 @@ sbatch slurm/run_rtx6000.sbatch python train.py --config-name=train_prior.yaml \
     seed=0
 
 
-
+# for debugging
 python train.py --config-name=train_prior.yaml \
     task=metaworld_ml45_prise \
     algo=bc_transformer \
     exp_name=bc_debug \
     variant_name=block_10 \
-    training.use_tqdm=false \
+    training.use_tqdm=true \
     training.use_amp=true \
     training.save_all_checkpoints=false \
     train_dataloader.persistent_workers=true \
     train_dataloader.num_workers=6 \
-    make_unique_experiment_dir=false \
+    make_unique_experiment_dir=true \
     algo.skill_block_size=1 \
     algo.frame_stack=10 \
     training.n_epochs=200 \
-    training.resume=true \
+    training.resume=false \
     logging.mode=disabled \
     training.do_profile=True \
     task.env_runner.debug=true \
